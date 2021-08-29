7620d62cedaafa19b62e58e5d3e90f91d9fd25c4
Amir Azis <71813621+amirazis@users.noreply.github.com>
2021-08-29 23:42:10 +0800

    Initial commit

---
+++ src/test/java/com/nexify/book/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexify.book.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
